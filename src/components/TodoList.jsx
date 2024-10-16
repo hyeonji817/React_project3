b532f9b8ddfde3ed2d96b52ace31b663775e8cb2
hyeonji817 <guswl930817@gmail.com>
2024-10-16 16:24:12 +0900

    리스트 페이지 생성 및 TodoList 페이지 import 구동

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,6 @@
+import "./TodoList.css"; 
+import { useState, useRef } from "react";
+import Header2 from "./components/Header2";
+import Editor2 from "./components/Editor2";
+import List from "./components/List";
+
