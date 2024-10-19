508cabb607db64183a4213bc0342edb96eef9288
hyeonji817 <guswl930817@gmail.com>
2024-10-19 15:37:53 +0900

    감정다이어리 코드복습 4차

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,118 @@
 import "./App.css";
+import { useReducer, useRef, createContext } from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
+import TodoList from "./components/TodoList";
 import Notfound from "./pages/Notfound";
+import { getEmotionImage } from "./util/get-emotion-image";
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date("2024-08-12").getTime(),
+    emotionId: 1, 
+    content: "1번 일기 내용", 
+  },
+  {
+    id: 2, 
+    createdDate: new Date("2024-08-11").getTime(),
+    emotionId: 2, 
+    content: "2번 일기 내용", 
+  },
+  {
+    id: 3, 
+    createdDate: new Date("2024-08-17").getTime(),
+    emotionId: 3, 
+    content: "3번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  switch(action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) => 
+        String(item.id) === String(action.data.id) // 여기서 오타남(큰 에러의 주원인인듯)
+          ? action.data 
+          : item
+      );
+    case "DELETE":
+      return state.filter(
+        (item) => String(item.id) !== String(action.id)
+      );
+    default:
+      return state;
+  }
+}
+
+export const DiaryStateContext = createContext(); 
+export const DiaryDispatchContext = createContext();
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지 
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지 
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지 
 function App() {
-  const nav = useNavigate(); 
+  const [data, dispatch] = useReducer(reducer, mockData); 
+  const idRef = useRef(3); 
 
-  const onClickButton = () => {
-    new("/new");
+  // 새로운 일기 추가 
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch ({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate, 
+        emotionId, 
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 수정 
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 삭제 
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    });
   };
 
   return (
     <>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+          value={{
+            onCreate,
+            onUpdate,
+            onDelete,
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="/todolist" element={<TodoList />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   );
 }
